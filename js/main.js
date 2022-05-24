22d893fb0ab179fb880f1b807c8462900ebfeb57
jrlopez185 <79105416+jrlopez185@users.noreply.github.com>
2022-05-24 09:16:50 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,3 +22,25 @@ function ac_carpeta() {
 vt_bts.addEventListener('click' ,ac_carpeta);
 
 
+//Funcion para s_l
+function CerrarConX() {
+    var x = document.getElementById("v_carp");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+s_l.addEventListener('click' ,CerrarConX);
+
+//Funcion para q_t
+function CerrarConX2() {
+    var x = document.getElementById("v_carp");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+q_t.addEventListener('click' ,CerrarConX2);
+
